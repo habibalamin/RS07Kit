c0566c9a57db84018c47fe0f68cc88b073db010a
Habib Alamin <ha.alamin@gmail.com>
2014-01-30 03:39:32 +0000

    Added RS07Prices header and implementation files.

---
+++ RS07Kit/RS07Prices/RS07Prices.m
@@ -0,0 +1,13 @@
+//
+//  RS07Prices.m
+//  RS07Kit
+//
+//  Created by Habib A on 30/01/2014.
+//  Copyright (c) 2014 alAmin Apps. All rights reserved.
+//
+
+#import "RS07Prices.h"
+
+@implementation RS07Prices
+
+@end
