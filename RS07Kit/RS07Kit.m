871b73222458721e7b6fa9b8ef061a56b205bcbd
Habib Alamin <ha.alamin@gmail.com>
2014-01-30 01:24:34 +0000

    Initial Commit

---
+++ RS07Kit/RS07Kit.m
@@ -0,0 +1,13 @@
+//
+//  RS07Kit.m
+//  RS07Kit
+//
+//  Created by Habib A on 30/01/2014.
+//  Copyright (c) 2014 alAmin Apps. All rights reserved.
+//
+
+#import "RS07Kit.h"
+
+@implementation RS07Kit
+
+@end
---
+++ RS07KitTests/RS07KitTests.m
@@ -0,0 +1,34 @@
+//
+//  RS07KitTests.m
+//  RS07KitTests
+//
+//  Created by Habib A on 30/01/2014.
+//  Copyright (c) 2014 alAmin Apps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RS07KitTests : XCTestCase
+
+@end
+
+@implementation RS07KitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
